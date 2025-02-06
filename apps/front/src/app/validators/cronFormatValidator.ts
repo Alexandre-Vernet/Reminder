1fc543eee9125eb9d58d9603306eb6076dc9e787
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-02-06 19:12:57 +0100

    fix(cron): Allow '?'

--- apps/front/src/app/validators/cronFormatValidator.ts
+++ apps/front/src/app/validators/cronFormatValidator.ts
@@ -13,7 +13,7 @@ export const cronFormatValidator = (): ValidatorFn => {
 		const cronExpressionLength = cronExpression.length;
 
 		// Cron must contain only numbers and special characters
-		const regex = new RegExp(/^[0-9\/*,-]+$/);
+		const regex = new RegExp(/^[0-9\/*,? -]+$/);
 		for (let i = 0; i < cronExpressionLength; i++) {
 			if (!regex.test(cronExpression[i])) {
 				return { cronFormatInvalidContainsLetters: true };
